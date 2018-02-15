ec7d5e06f70ee94d2b4201d245024db18a639487
John Ankarström <92702+jocap@users.noreply.github.com>
2018-02-15 17:00:32 +0100

    Revert "Gave up on TypeScript ..."
    This reverts commit 9673856452f5f351bfe94bb765ca06908e73bd81.

--- client/static/js/quarter.js
+++ client/static/js/quarter.js
@@ -1,30 +1,30 @@
-// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
-
 "use strict";
-
+// TODO: Add license
+exports.__esModule = true;
+var CryptoJS = require("crypto-js");
 // -> null => error shown to user
 function encrypt(object, key) {
     if (key === '') {
-        console.error('Error: key cannot be empty.')
-        return null
+        console.error('Error: key cannot be empty.');
+        return null;
     }
-    return CryptoJS.AES.encrypt(JSON.stringify(object), key)
+    return CryptoJS.AES.encrypt(JSON.stringify(object), key);
 }
-
 // -> null => discrete error shown to user, optionally
 function decrypt(cipher, key) {
     if (cipher === null) {
-        console.error('Error: cipher cannot be null.')
-        return null
+        console.error('Error: cipher cannot be null.');
+        return null;
     }
     try {
-        var bytes = CryptoJS.AES.decrypt(cipher.toString(), key)
-        return JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+        var bytes = CryptoJS.AES.decrypt(cipher.toString(), key);
+        return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
     }
     catch (SyntaxError) {
-        console.error('Error: cipher could not be decrypted.')
-        return null
+        console.error('Error: cipher could not be decrypted.');
+        return null;
     }
 }
-
-// @license-end
+var cipher = encrypt('test', 'key');
+var object = decrypt(cipher, 'key');
+console.log(object);
